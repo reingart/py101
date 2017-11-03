43157d9d1dfa1c73c77ea7ee491f240f44041d44
Mariano Reingart <reingart@gmail.com>
2017-11-03 04:04:48 -0300

    base Runestone Workshop 2017
    https://github.com/RunestoneInteractive/Workshop2017.git

---
+++ pavement.py
@@ -0,0 +1,63 @@
+import paver
+from paver.easy import *
+import paver.setuputils
+paver.setuputils.install_distutils_tasks()
+import os, sys
+from runestone.server import get_dburl
+from sphinxcontrib import paverutils
+import pkg_resources
+from socket import gethostname
+
+sys.path.append(os.getcwd())
+
+home_dir = os.getcwd()
+project_name = "sigcse2017"
+
+master_url = None
+doctrees = None
+
+if master_url is None:
+    if gethostname() in ['web407.webfaction.com', 'rsbuilder']:
+        master_url = 'http://interactivepython.org'
+        if os.path.exists('../../custom_courses/{}'.format(project_name)):
+            doctrees = '../../custom_courses/{}/doctrees'.format(project_name)
+        else:
+            doctrees = './build/{}/doctrees'.format(project_name)
+    else:
+        master_url = 'http://127.0.0.1:8000'
+        doctrees = './build/{}/doctrees'.format(project_name)
+
+
+master_app = 'runestone'
+serving_dir = "./build/sigcse2017"
+dest = "../../static"
+
+options(
+    sphinx = Bunch(docroot=".",),
+
+    build = Bunch(
+        builddir="./build/sigcse2017",
+        sourcedir="_sources",
+        outdir="./build/sigcse2017",
+        confdir=".",
+        project_name=project_name,
+        template_args={'course_id': 'sigcse2017',
+                       'login_required':'false',
+                       'appname':master_app,
+                       'loglevel': 10,
+                       'course_url':master_url,
+                       'use_services': 'true',
+                       'python3': 'true',
+                       'dburl': '',
+                       'basecourse': 'sigcse2017'
+                        }
+    )
+)
+
+version = pkg_resources.require("runestone")[0].version
+options.build.template_args['runestone_version'] = version
+
+# If DBUSER etc. are in the environment override dburl
+options.build.template_args['dburl'] = get_dburl(outer=locals())
+
+from runestone import build  # build is called implicitly by the paver driver.
